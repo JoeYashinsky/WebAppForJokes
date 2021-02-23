81f573c0b3b36f0bd406ce1690ebc1882530280d
JoeYashinsky <joey.yashinsky@gmail.com>
2021-02-23 15:06:04 -0500

    background color change made in css file within wwwroot. Authorize decorator also added to the necessary action forms in the JokesController

--- JokesWebApp/Controllers/JokesController.cs
+++ JokesWebApp/Controllers/JokesController.cs
@@ -56,8 +56,8 @@ namespace JokesWebApp.Controllers
             return View(joke);
         }
 
-        [Authorize]
         // GET: Jokes/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -81,6 +81,7 @@ namespace JokesWebApp.Controllers
         }
 
         // GET: Jokes/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -99,6 +100,7 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Setup,Punchline")] Joke joke)
@@ -132,6 +134,7 @@ namespace JokesWebApp.Controllers
         }
 
         // GET: Jokes/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -152,6 +155,7 @@ namespace JokesWebApp.Controllers
         // POST: Jokes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var joke = await _context.Joke.FindAsync(id);
